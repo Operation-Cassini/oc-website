0be8a0dbb4b14da8587e05339dd93d9be875c5a4
amanda-tu <avtu@ucdavis.edu>
2024-04-23 13:52:25 -0700

    Finished HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import NextButton from './components/NextButton'; // Import your NextButton component
-import InstructionContainer from './components/InstructionContainer'; 
-import WordSelectionButton from './components/WordSelectionButton';
+import saturn from './saturn.png';
 import './App.css';
 
 const App = () => {
   const handleNext = () => {
     // Handle next button click event
   };
-  const instructions = [
-    "This is an instruction. You can pass any text here.",
-    "This is another instruction. You can pass any text here.",
-    "This is another instruction. You can pass any text here."
-  ];
-
 
   return (
-    <div>
-      <h1>Your React App</h1>
-      <InstructionContainer instructions={instructions} />
-      <NextButton onClick={handleNext}>NEXT</NextButton>
-      <WordSelectionButton>JAZZ</WordSelectionButton>
+    <div className="container">
+      <div className="SATURN-png">
+        <img src={saturn} alt="saturn logo" />
+      </div>
+      <div className="start-button-container">
+        <NextButton>START</NextButton>
+      </div>
     </div>
   );
 };
