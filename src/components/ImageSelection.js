3118d07b2e831eaecfafbdc82a1d368c5f285311
amanda-tu <avtu@ucdavis.edu>
2024-05-17 15:58:13 -0700

    Fixed image selection

--- src/components/ImageSelection.js
+++ src/components/ImageSelection.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import './ImageSelection.css';
 
-const ImageSelection = ({ images, rows, cols }) => {
+const ImageSelection = ({ images, rows, cols, onImageClick }) => {
   const containerStyle = {
     display: 'flex',
     flexWrap: 'wrap',
     gap: '10px', // Adjust the gap between images if needed
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start', // Align images to the start to prevent extra space at the end
+  };
+
+
+  const handleImageClick = (index) => {
+    console.log('Clicked image index:', index);
   };
 
   const generateImages = () => {
-    console.log("generate images");
     let imageElements = [];
-    console.log(rows);
-    console.log(cols);
     for (let i = 0; i < rows * cols; i++) {
       let image = images[i] || ''; // Get the image for the current position, empty string if none
-      console.log("Image file path: ", image);
       imageElements.push(
         <div
           key={i}
           className="image-container"
           style={{
-            flexBasis: `calc(100% / ${cols} - 10px)`, // Adjust the width of each image container
+            flexBasis: `calc((100% / ${cols}) - 10px)`, // Adjust the width of each image container
+            boxSizing: 'border-box', // Include padding and border in the element's total width and height
           }}
         >
-          {image && <img src={image} alt={`Selected ${i}`} className="image" />}
+          {image && (
+            <button
+              onClick={() => handleImageClick(i)}
+              className="image-button"
+              style={{
+                border: 'none',
+                background: 'none',
+                padding: 0,
+                cursor: 'pointer',
+                width: '100%', // Ensure the button takes full width of the container
+                height: '100%', // Ensure the button takes full height of the container
+              }}
+            >
+              <img src={image} alt={`Selected ${i}`} className="image" style={{ width: '100%', height: 'auto' }} />
+            </button>
+          )}
         </div>
       );
     }
