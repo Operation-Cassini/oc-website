dbfdbe45bd01cdacfa3bc14a8f3c046e07b45453
kalechip22 <kayla.hom22@gmail.com>
2024-06-11 13:30:30 -0700

    added to package.json so it compiles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { Amplify } from "aws-amplify";
-// import config from "./aws-exports";
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json';
 
-// Amplify.configure(config);
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
