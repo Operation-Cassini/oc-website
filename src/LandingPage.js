67cfd9eea0c89b80fa28afcb51822788e3ddcadf
dbissig <dbissig@gmail.com>
2025-01-02 15:48:45 -0800

    update technical info

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -130,9 +130,12 @@ const LandingPage = () => {
                   This version does not ask what state someone is in, simply awards that point (+1). <b>A perfect score is "29+1 / 30".</b>  <br />
                   <br />The original program is fully in the public domain, and validated against the Montreal Cognitive Assessment (MoCA) (PMC7771179). <br />
                   <br />
-                  This version was inspired by Dr. Assecondi's browser-based version on the Prolific platform (PMC10533744), 
-                  and developed by Kayla Rose Hom, Eric Kho, Emily Loh, & Amanda Tu as a capstone project for ECS193AB @ University of California - Davis. 
-                  The developers, along with David Bissig, defer to UC Davis on copyright matters because this was developed as part of a class assignment.<br />  
+                  This site is inspired by Dr. Assecondi's browser-based version on the Prolific platform (PMC10533744). Her group found that "limited assistance with entering responses" does not impact test scores (PMC11557479).
+                  This site was initially developed by Kayla Rose Hom, Eric Kho, Emily Loh, & Amanda Tu as a capstone project for ECS193AB @ University of California - Davis. 
+                  Those developers, along with David Bissig, defer to UC Davis on copyright matters because this was developed as part of a class assignment. 
+                  Kayla and Emily further refined this site, resulting in the latest version.<br />
+                  <br />
+                  Main contributors to <b>draft</b> non-English versions: Anna Troung (Vietnamese), Dr. Gabriel Moreno (Spanish), Dr. Jihey Yuk (Korean), Dr. Clarice Zhou (Smplified Chinese), Dr. Chao-Yi Wu (Traditional Chinese).
                   
                 </span>
               )}
